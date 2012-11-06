d6183ba634d4e40ccde787aef1a9036a481c4673
Yusuke Wada <yusuke@kamawada.com>
2012-11-06 13:54:14 +0900

    ID -> No.

--- lib/Profile81/Web/Controller/Root.pm
+++ lib/Profile81/Web/Controller/Root.pm
@@ -11,8 +11,10 @@ sub search {
     my $self = shift;
     my $id = $self->req->param('id');
     my $user = $self->db->single('user', { id => $id });
-    return $self->render_not_found unless $user;
-    $self->redirect_to('/' . $user->id);
+    return $self->redirect_to('/' . $user->id) if $user;
+    $user = $self->db->single('user', { screen_name => $id });
+    return $self->redirect_to('/' . $user->id) if $user;
+    return $self->render_not_found;
 }
 
 1;
